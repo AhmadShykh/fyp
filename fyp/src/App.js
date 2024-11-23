b992556b152fae34e73f5a9acc445699bbb1f982
Manas1255 <anas1255.ma@gmail.com>
2024-11-24 01:01:01 +0500

    latest changes

---
+++ fyp/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import HomePage from "./pages/HomePage";
+import ProcessPage from "./pages/ProcessPage";
+import SideBar from "./components/SideBar";
+
+const App = () => {
+  return (
+    <div>
+      {/* <HomePage /> */}
+      {/* <ProcessPage /> */}
+      <SideBar />
+    </div>
+  );
+};
+
+export default App;
